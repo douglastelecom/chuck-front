fd3163dbad3ce2ae15ba1a057c03410e5659a0e3
Douglas <douglastelec@gmail.com>
2024-07-02 21:35:49 -0300

    fix: url svg picturies

--- src/components/Cards/Cards.tsx
+++ src/components/Cards/Cards.tsx
@@ -22,12 +22,12 @@ export default function Cards({setShowPopup, setPopupDescription, setPopupType}:
       <div className="container mx-auto p-4">
       <div className="flex flex-wrap justify-center">
         <div onClick={onclickread} className="hover:cursor-pointer bg-white shadow-lg rounded-lg p-6 m-5 w-full sm:w-1/2 lg:w-1/3 flex flex-col items-center justify-center">
-          <img className="h-1/4 w-1/4" src="src/assets/svg/interrogation.svg" alt="Ícone SVG"/>
+          <img className="h-1/4 w-1/4" src="./src/assets/svg/interrogation.svg" alt="Ícone SVG"/>
           <h2 className="text-xl font-semibold mb-2 text-center text-black">Ler uma curiosidade</h2>
           <p className="text-gray-700 text-center">Conheça algumas das hitórias mais incríveis do mestre Norris...</p>
         </div>
         <div onClick={onclickwrite} className="hover:cursor-pointer bg-white shadow-lg rounded-lg p-6 m-5 w-full sm:w-1/2 lg:w-1/3 flex flex-col items-center justify-center">
-        <img className="h-1/4 w-1/4" src="src/assets/svg/hand-and-pen.svg" alt="Ícone SVG"/>
+        <img className="h-1/4 w-1/4" src="./src/assets/svg/hand-and-pen.svg" alt="Ícone SVG"/>
           <h2 className="text-xl font-semibold mb-2 text-center text-black">Cadastrar uma curiosidade</h2>
           <p className="text-gray-700 text-center">Sabe de algo novo? Conte-nos!</p>
         </div>
